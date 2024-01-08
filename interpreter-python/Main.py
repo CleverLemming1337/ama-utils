c4320131c6dd51d090abe65037c88cab0578ebe0
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-01-08 18:34:08 +0000

    0.3.0

--- interpreter-python/Main.py
+++ interpreter-python/Main.py
@@ -45,7 +45,52 @@ class Stack:
         self.values.append(value)
     def pop(self):
         return self.values.pop()
+
+def add(r1, r2, r3):
+    reg.setValue(r3, reg.getValue(r1)+reg.getValue(r2))
+
+def sub(r1, r2, r3):
+    reg.setValue(r3, reg.getValue(r1)-reg.getValue(r2))
+
+def mult(r1, r2, r3):
+    reg.setValue(r3, reg.getValue(r1)*reg.getValue(r2))
+
+def lr():
+    raise AMACommandWarning(None, "Not yet implemented")
+lm = lr
+
+def mov(r1, r2):
+    reg.setValue(r2, reg.getValue(r1))
+    reg.setValue(r1, 0)
+
+def _set(r, val):
+    reg.setValue(r, val)
+
+def cmov(r1, r2):
+    if flag:
+        reg.setValue(r2, reg.getValue(r1))
+
+setm = lr
+
+def nf():
+    flag = not flag
+
+def sfl(r1, r2):
+    flag = reg.getValue(r1) < reg.getValue(r2)
+
+def sfg(r1, r2):
+    flag = reg.getValue(r1) > reg.getValue(r2)
+
+def sfe(r1, r2):
+    flag = reg.getValue(r1) == reg.getValue(r2)
     
+ext = lm
+
+flag = False
+reg = Memory()
+mem = Memory()
+stack = Stack()
+commandFuncs = {"push":stack.push, "pop":stack.pop, "add":add, "sub":sub, "mult":mult, "lr":lr, "lm":lm, "mov":mov, "set":_set, "cmov":cmov, "setm":setm, "nf":nf, "sfl":sfl, "sfg":sfg, "sfe":sfe, "ext":ext}
 # commands = ["push pop add sub mult lr lm mov set cmov setm nf sfl sfg sfe ext".split()]
 commands = {"push":(1,), "pop":(1,), "add":(1, 1, 1), "sub":(1, 1, 1), "mult":(1, 1, 1), "lr":(1, 2), "lm":(2, 1), "mov":(1, 1), \
                    "set":(1, 4), "cmov":(1, 1), "setm":(2, 3), "nf":(), "sfl":(1, 1), "sfg":(1, 1), "sfe":(1, 1), "ext":(5, 1)} # for each element of command a tuple of integers with argument types (see doc)
@@ -82,7 +127,7 @@ def checkSyntax(lines):
             aTExp = commands[command][jindex] # number of arg tyoe expected
             aTGot = j[0] # char (:, !) of argType got
             try:
-                aVal = int(j[1:])
+                aVal = int(j[1:], 16)
             except ValueError:
                 raise AMACommandError(index+1, f"Invalid value for argument nr. {jindex+1}: {aVal}")
 
@@ -95,25 +140,34 @@ def checkSyntax(lines):
     return instructions
 
 def parseLine(line):
-    pass
+    print(line)
 
 def parseLines(lines):
     try:
         instructions = checkSyntax(lines)
         nInstructions = len(instructions)
+        print(instructions, nInstructions)
+        while True:
+            if reg.getValue(0) >= nInstructions:
+                exit(0)
+            result = parseLine(instructions[reg.getValue(0)])
+            if result != None:
+                reg.setItem(0, result)
+            else:
+                reg.setItem(0, reg.getValue(0)+1)
     except AMAError as err:
+        if err.line == None:
+            err.line = reg.getValue(0)
         print(f"\033[31m{err.__class__.__name__}: Line {err.line}: {err.text}\033[0m")
         exit(1)
     except AMAWarning as warn:
+        if warn.line == None:
+            warn.line = reg.getValue(0)
         print(f"\033[33m{warn.__class__.__name__}: Line {warn.line}: {warn.text}\033[0m")
         
-version = "0.2.0"
+version = "0.3.0"
 specVersion = "1.4"
 
-flag = False
-reg = Memory()
-mem = Memory()
-stack = Stack()
 
 if len(sys.argv) < 2:
     print(f"AMA interpreter (Python) {version} (for spec version {specVersion})\nCopyright (c) CleverLemming1337")
