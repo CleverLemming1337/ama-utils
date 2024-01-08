7cc6a0757c28008b2558363c4c16ad45bfce87d0
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-01-08 16:56:03 +0000

    v0.2

--- interpreter-python/Main.py
+++ interpreter-python/Main.py
@@ -1,3 +1,5 @@
+import sys
+
 class AMAError(Exception):
     def __init__(self, line, text):
         self.line = line
@@ -26,10 +28,28 @@ class AMAWarning(Warning):
 class AMACommandWarning(AMAWarning):
     pass
 
+class Memory:
+    def __init__(self):
+        self.values = dict()
+
+    def setValue(self, nr, value):
+        self.values[nr] = value
+
+    def getValue(self, nr):
+        return self.values[nr] if nr in self.values else 0
+
+class Stack:
+    def __init__(self):
+        self.values = []
+    def push(self, value):
+        self.values.append(value)
+    def pop(self):
+        return self.values.pop()
+    
 # commands = ["push pop add sub mult lr lm mov set cmov setm nf sfl sfg sfe ext".split()]
 commands = {"push":(1,), "pop":(1,), "add":(1, 1, 1), "sub":(1, 1, 1), "mult":(1, 1, 1), "lr":(1, 2), "lm":(2, 1), "mov":(1, 1), \
-                   "set":(1, 3), "cmov":(1, 1), "setm":(2, 3), "nf":(), "sfl":(1, 1), "sfg":(1, 1), "sfe":(1, 1), "ext":(4, 1)} # for each element of command a tuple of integers with argument types (see doc)
-argTypes = {1:(":", 255), 2:(":", 4_294_967_296), 3:("!", 255), 4:("!", 4_294_967_296), 5:(":", 4)}
+                   "set":(1, 4), "cmov":(1, 1), "setm":(2, 3), "nf":(), "sfl":(1, 1), "sfg":(1, 1), "sfe":(1, 1), "ext":(5, 1)} # for each element of command a tuple of integers with argument types (see doc)
+argTypes = {1:(":", 255), 2:("%", 4_294_967_296), 3:("$", 255), 4:("!", 4_294_967_296), 5:("%", 4)}
 argTypeNames = {1:"register address", 2:"memory address", 3:"memory value", 4:"register value", 5:"external command"}
 def checkSyntax(lines):
     """
@@ -55,18 +75,51 @@ def checkSyntax(lines):
         if argsGot < argsExp:
             raise AMACommandError(index+1, f"Expected {argsExp} arguments but got {argsGot}")
         if argsGot > argsExp:
-            raise AMACommandWarning(index+1, f"Too many arguments: {argsGot} instead of {argsExp}. Ignoring {', '.join(args[argsGot-argsExp:])}")
+            raise AMACommandWarning(index+1, f"Too many arguments: {argsGot} instead of {argsExp}. Ignoring last one(s): {', '.join(args[argsGot+1-argsExp:])}")
         
         for jindex, j in enumerate(args):
-            aTExp = argTypes[command][0] # number of arg tyoe expected
+            
+            aTExp = commands[command][jindex] # number of arg tyoe expected
             aTGot = j[0] # char (:, !) of argType got
+            try:
+                aVal = int(j[1:])
+            except ValueError:
+                raise AMACommandError(index+1, f"Invalid value for argument nr. {jindex+1}: {aVal}")
 
-            # if aTExp != aTGot:
-            #     raise AMACommandError(index+1, f"Argument nr. {jindex+1} must be {argTypeNames[]}")
+            if argTypes[aTExp][0] != aTGot:
+                raise AMACommandError(index+1, f"Argument nr. {jindex+1} must be {argTypeNames[aTExp]}")
+            
+            if aVal > argTypes[aTExp][1]:
+                raise AMACommandError(index+1, f"Too high value for argument nr. {jindex+1}: {aVal}")
+    
+    return instructions
 
-        
+def parseLine(line):
+    pass
 
+def parseLines(lines):
+    try:
+        instructions = checkSyntax(lines)
+        nInstructions = len(instructions)
+    except AMAError as err:
+        print(f"\033[31m{err.__class__.__name__}: Line {err.line}: {err.text}\033[0m")
+        exit(1)
+    except AMAWarning as warn:
+        print(f"\033[33m{warn.__class__.__name__}: Line {warn.line}: {warn.text}\033[0m")
+        
+version = "0.1.0"
+specVersion = "1.4"
 
-        
-        
-        
+flag = False
+reg = Memory()
+mem = Memory()
+stack = Stack()
+
+if len(sys.argv) < 2:
+    print(f"AMA interpreter (Python) {version} (for spec version {specVersion})\nCopyright (c) CleverLemming1337")
+    while True:
+        parseLines([input("AMA CLI >>> ")])
+else:
+    with open(sys.argv[1], "r") as f:
+        parseLines(f.readlines())
+
--- interpreter-python/docs/code.md
+++ interpreter-python/docs/code.md
@@ -6,9 +6,9 @@ AMA has different types of arguments, in the code represented by integers:
 | Name | Code | Integer | Max Value |
 | ---- | ---- | ------- | --------- |
 | Register | :r | 1 | 255 |
-| Memory   | :radr | 2 | 4,294,967,296 (2^32) |
-| Value (Memory) | !val | 3 | 255 |
-| Value (Register) | !val | 4,294,967,296 (2^32) |
+| Memory   | %radr | 2 | 4,294,967,296 (2^32) |
+| Value (Memory) | $val | 3 | 255 |
+| Value (Register) | !val | 4 | 4,294,967,296 (2^32) |
 | External | :rop | 4 | 4 |
 
 ## Errors
@@ -20,3 +20,40 @@ This error occurs when:
 - it's tried to change register 256+ (not existing)
 
 ### `AMASyntaxError`
+This is not implemented yet.
+
+### `AMACommandError`
+This error is thrown when:
+- an unknown command is used
+- not enough arguments were given
+- wrong argument type were given
+
+## Warnings
+There are also some warnings, which don't stop the program:
+
+### `AMACommandWarning`
+This warning is Thrown, when:
+- too many arguments were given
+
+## Functions
+
+### `checkSyntax(lines: list)`
+This function checks the syntax of the given lines.
+It throws errors or warnings in case of wrong code.
+
+## Other classes
+### `Memory`
+This class represents the memory (also the register) with a dictionary as values.
+#### Attributes
+- `values: dict`: The values saved in the memory (or register)
+#### Methods
+- `setValue(nr: int, value: int)`: Sets the `nr`th Element of the `values` dict to `nr`
+- `getValue(nr: int) -> int`: Returns the `nr`th element of the `values` dict or 0 if not set.
+
+### `Stack`
+This class represents a stack.
+#### Attributes
+- `values: list`: A list of the values (with the first element at 0)
+#### Methods
+- `push(value: int)`: Appends `value` to `values`
+- `pop() -> int`: Returns `values`' last element and removes it
